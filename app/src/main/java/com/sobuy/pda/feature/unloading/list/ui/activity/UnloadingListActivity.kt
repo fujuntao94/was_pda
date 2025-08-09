27a6977b6bd298d4d2677703c052117a947fe31c
fujuntao <18258399256@163.com>
2025-08-09 21:29:54 +0800

    1

--- app/src/main/java/com/sobuy/pda/feature/unloading/list/ui/activity/UnloadingListActivity.kt
+++ app/src/main/java/com/sobuy/pda/feature/unloading/list/ui/activity/UnloadingListActivity.kt
@@ -30,100 +30,100 @@ class UnloadingListActivity :
 
     override fun initListeners() {
         super.initListeners()
-        binding.onlyInLoadingTab.setOnClickListener {
-            if (activeTab == TAB_FIRST) {
-                setTabActive(
-                    activeTab = binding.onlyInLoadingTab,
-                    activeText = binding.onlyInLoadingText,
-                    inactiveTab = binding.allDesksTab,
-                    inactiveText = binding.allDesksText
-                )
-                activeTab = TAB_SECOND
-                binding.asyncButton.visibility = View.GONE
-            }
-        }
-
-        binding.allDesksTab.setOnClickListener {
-            if (activeTab == TAB_SECOND) {
-                setTabActive(
-                    activeTab = binding.allDesksTab,
-                    activeText = binding.allDesksText,
-                    inactiveTab = binding.onlyInLoadingTab,
-                    inactiveText = binding.onlyInLoadingText
-                )
-                activeTab = TAB_FIRST
-                binding.asyncButton.visibility = View.VISIBLE
-            }
-        }
+//        binding.onlyInLoadingTab.setOnClickListener {
+//            if (activeTab == TAB_FIRST) {
+//                setTabActive(
+//                    activeTab = binding.onlyInLoadingTab,
+//                    activeText = binding.onlyInLoadingText,
+//                    inactiveTab = binding.allDesksTab,
+//                    inactiveText = binding.allDesksText
+//                )
+//                activeTab = TAB_SECOND
+//                binding.asyncButton.visibility = View.GONE
+//            }
+//        }
+//
+//        binding.allDesksTab.setOnClickListener {
+//            if (activeTab == TAB_SECOND) {
+//                setTabActive(
+//                    activeTab = binding.allDesksTab,
+//                    activeText = binding.allDesksText,
+//                    inactiveTab = binding.onlyInLoadingTab,
+//                    inactiveText = binding.onlyInLoadingText
+//                )
+//                activeTab = TAB_FIRST
+//                binding.asyncButton.visibility = View.VISIBLE
+//            }
+//        }
     }
 
-    override fun initViews() {
-        super.initViews()
-        topRecyclerView = binding.topRecyclerView
-        topRecyclerView.itemAnimator = null
-        var d10 = resources.getDimensionPixelSize(R.dimen.d10)
-        topRecyclerView.layoutManager = GridLayoutManager(this, SPAN_COUNT)
-        topRecyclerView.addItemDecoration(
-            GridSpacingItemDecoration(
-                spanCount = SPAN_COUNT,
-                horizontalSpacing = d10,
-                verticalSpacing = d10,
-                includeEdge = false
-            )
-        )
-
-        val adapter = UnloadingListTopItemAdapter()
-        topRecyclerView.adapter = adapter.apply {
-            setOnClickListener { item ->
-                val position = adapter.currentList.indexOfFirst { it.id == item.id }
-                viewModel.selectItem(position)
-            }
-        }
-
-        viewModel.itemList.observe(this) { items ->
-            // 控制空状态显示
-            if (items.isEmpty()) {
-                binding.content.visibility = View.GONE
-                binding.emptyStateView.visibility = View.VISIBLE
-            } else {
-                binding.content.visibility = View.VISIBLE
-                binding.emptyStateView.visibility = View.GONE
-            }
-
-            adapter.submitList(items)
-        }
-
-        childRecyclerView = binding.childRecyclerView
-//        childRecyclerView.itemAnimator = null
-        childRecyclerView.layoutManager = GridLayoutManager(this, SPAN_COUNT)
-        childRecyclerView.addItemDecoration(
-            GridSpacingItemDecoration(
-                spanCount = SPAN_COUNT,
-                horizontalSpacing = d10,
-                verticalSpacing = d10,
-                includeEdge = false
-            )
-        )
-
-        val childrenItemAdapter = UnloadingListChildrenItemAdapter()
-        childRecyclerView.adapter = childrenItemAdapter.apply {
-            setOnClickListener { item ->
-                Log.d(TAG, "jumperDetail: $item")
-                startActivity(UnloadingDetailActivity::class.java)
-            }
-        }
-
-        // Key: Observe the changes in the selected sub-data and update the sub-item list in real time
-        viewModel.selectedChildren.observe(this) { selectedChildren ->
-
-            if (selectedChildren.isEmpty()) {
-                binding.childRecyclerView.visibility = View.GONE
-            } else {
-                binding.childRecyclerView.visibility = View.VISIBLE
-            }
-            childrenItemAdapter.submitList(selectedChildren)
-        }
-    }
+//    override fun initViews() {
+//        super.initViews()
+//        topRecyclerView = binding.topRecyclerView
+//        topRecyclerView.itemAnimator = null
+//        var d10 = resources.getDimensionPixelSize(R.dimen.d10)
+//        topRecyclerView.layoutManager = GridLayoutManager(this, SPAN_COUNT)
+//        topRecyclerView.addItemDecoration(
+//            GridSpacingItemDecoration(
+//                spanCount = SPAN_COUNT,
+//                horizontalSpacing = d10,
+//                verticalSpacing = d10,
+//                includeEdge = false
+//            )
+//        )
+//
+//        val adapter = UnloadingListTopItemAdapter()
+//        topRecyclerView.adapter = adapter.apply {
+//            setOnClickListener { item ->
+//                val position = adapter.currentList.indexOfFirst { it.id == item.id }
+//                viewModel.selectItem(position)
+//            }
+//        }
+//
+//        viewModel.itemList.observe(this) { items ->
+//            // 控制空状态显示
+//            if (items.isEmpty()) {
+//                binding.content.visibility = View.GONE
+//                binding.emptyStateView.visibility = View.VISIBLE
+//            } else {
+//                binding.content.visibility = View.VISIBLE
+//                binding.emptyStateView.visibility = View.GONE
+//            }
+//
+//            adapter.submitList(items)
+//        }
+//
+//        childRecyclerView = binding.childRecyclerView
+////        childRecyclerView.itemAnimator = null
+//        childRecyclerView.layoutManager = GridLayoutManager(this, SPAN_COUNT)
+//        childRecyclerView.addItemDecoration(
+//            GridSpacingItemDecoration(
+//                spanCount = SPAN_COUNT,
+//                horizontalSpacing = d10,
+//                verticalSpacing = d10,
+//                includeEdge = false
+//            )
+//        )
+//
+//        val childrenItemAdapter = UnloadingListChildrenItemAdapter()
+//        childRecyclerView.adapter = childrenItemAdapter.apply {
+//            setOnClickListener { item ->
+//                Log.d(TAG, "jumperDetail: $item")
+//                startActivity(UnloadingDetailActivity::class.java)
+//            }
+//        }
+//
+//        // Key: Observe the changes in the selected sub-data and update the sub-item list in real time
+//        viewModel.selectedChildren.observe(this) { selectedChildren ->
+//
+//            if (selectedChildren.isEmpty()) {
+//                binding.childRecyclerView.visibility = View.GONE
+//            } else {
+//                binding.childRecyclerView.visibility = View.VISIBLE
+//            }
+//            childrenItemAdapter.submitList(selectedChildren)
+//        }
+//    }
 
     private val colorPrimary by lazy { ContextCompat.getColor(this, R.color.primary) }
     private val colorWhite by lazy { ContextCompat.getColor(this, R.color.white) }
--- app/src/main/java/com/sobuy/pda/widgets/impl/TopNavigationView.kt
+++ app/src/main/java/com/sobuy/pda/widgets/impl/TopNavigationView.kt
@@ -3,23 +3,32 @@ package com.sobuy.pda.widgets.impl
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.angcyo.tablayout.DslTabLayout
 import com.sobuy.pda.R
 
 class TopNavigationView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
+) : DrawerLayout(context, attrs, defStyleAttr) {
     private var textTitle: TextView
-    private var leftIcon: ImageView
+
+    private var navigationBar: View
     private var leftClickListener: (() -> Unit)? = null
+    // 抽屉容器（外部需传入DrawerLayout）
+//    private var drawerContent: LinearLayout
 
     init {
-        orientation = VERTICAL
+        layoutParams = LayoutParams(
+            LayoutParams.MATCH_PARENT,
+            LayoutParams.MATCH_PARENT,
+        )
 
         context.theme.obtainStyledAttributes(
             attrs,
@@ -29,6 +38,7 @@ class TopNavigationView @JvmOverloads constructor(
             try {
                 inflate(context, R.layout.custom_top_navigation_view, this@TopNavigationView)
 
+
                 textTitle = findViewById(R.id.title)
                 val labelText = getString(R.styleable.TopNavigationView_title)
                 textTitle.text = labelText ?: "Default"
@@ -37,20 +47,44 @@ class TopNavigationView @JvmOverloads constructor(
                     R.styleable.TopNavigationView_backgroundColor,
                     ContextCompat.getColor(context, R.color.primary)
                 )
-                setBackgroundColor(customBgColor)
+                navigationBar = findViewById(R.id.navigation_bar)
+                navigationBar.setBackgroundColor(customBgColor)
 
                 // Left return click event
-                leftIcon = findViewById(R.id.left)
-                leftIcon.setOnClickListener {
-                    Log.d(TAG, "setBackOnClickListener: back")
-                    leftClickListener?.invoke()
-                }
+//                leftIcon = findViewById(R.id.left)
+//                leftIcon.setOnClickListener {
+//                    Log.d(TAG, "setBackOnClickListener: back")
+//                    leftClickListener?.invoke()
+//                }
+//
+
             } finally {
                 recycle()
             }
         }
     }
 
+    /**
+     * 判断抽屉是否处于打开状态
+     */
+//    fun isDrawerOpen(): Boolean {
+//        // 检查抽屉容器是否处于打开状态
+//        return isDrawerOpen(drawerContent)
+//    }
+//
+//    // 关闭抽屉
+//    fun closeDrawer() {
+//        if (isDrawerOpen(drawerContent)) {
+//            closeDrawer(drawerContent)
+//        }
+//    }
+//
+//    // 打开抽屉
+//    fun openDrawer() {
+//        if (!isDrawerOpen(drawerContent)) {
+//            openDrawer(drawerContent)
+//        }
+//    }
     fun setBackOnClickListener(listener: () -> Unit) {
         leftClickListener = listener
     }
